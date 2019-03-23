2f4006df888af050de47d4cc94ce9925b3ca542a
Svsoetbaattd <33157279+Svsoetbaattd@users.noreply.github.com>
2019-03-23 17:40:30 -0400

    The Spline related shit

---
+++ Logan-Spline-Fuck-Wallstreet/Velociraptor/Assets/Resources/Scripts/CollisionChecker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionChecker : MonoBehaviour
+{
+    public bool col = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        col = true;
+    }
+}
---
+++ Logan-Spline-Fuck-Wallstreet/Velociraptor/Assets/Resources/Scripts/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int eggs, units, unitsMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            Debug.Log("Units - " + units + "/" + unitsMax);
+            Debug.Log("Eggs - " + eggs);
+        }
+    }
+}
