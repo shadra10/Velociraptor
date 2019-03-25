7fbe16b644be5135fc8ec86bb5d8600159399ba7
Svsoetbaattd <33157279+Svsoetbaattd@users.noreply.github.com>
2019-03-24 20:50:59 -0400

    Suck My Dick GitHub

---
+++ Assets/Resources/Scripts/FlockLeader.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockLeader : MonoBehaviour
+{
+    public int dmg;
+    public float t, attT;
+    public float range, attSpeed;
+    public float speed = 1.0F;
+    float time;
+    public bool isLeader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+        speed = 12;
+        //Parent
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (isLeader == true)
+        {
+            transform.Rotate(0, 1, 0);
+
+            if (time % 2 < 1)
+            {
+                //transform.Rotate(0, 0.5f, 0);
+                float angle = 0.25f;
+                Quaternion rot = this.transform.rotation;
+                rot = rot * Quaternion.Euler(0, angle, 0);
+                this.transform.rotation = rot;
+            }            //Transform trans;
+            else
+            {   
+                float angle = -2f;
+                Quaternion rot = this.transform.rotation;
+                rot = rot * Quaternion.Euler(0, angle, 0);
+                this.transform.rotation = rot;
+            }
+            Vector3 myvec = new Vector3(0, 0, speed * Time.deltaTime);
+            myvec = this.transform.rotation * myvec;
+
+            Vector3 vec = this.transform.position;
+            Rigidbody body = this.GetComponent<Rigidbody>();
+            body.AddForce(myvec, ForceMode.Impulse);
+            //this.transform.position = vec;
+        }
+        else
+        {
+            GameObject target = GetComponentInParent<Flock>().leader;
+            float total = 0;
+            float count = 0;
+            foreach (GameObject raptor in GetComponentInParent<Flock>().units)
+            {
+                
+                if (raptor!=null&&(this.transform.position - raptor.transform.position).magnitude < 5)
+                {
+                    Vector3 targetVector = raptor.transform.position - this.transform.position;
+                    Vector3 temp = new Vector3(0, 0, 1);
+                    temp = this.transform.rotation * temp;
+                    float angle = Vector3.SignedAngle(temp, targetVector, Vector3.up);
+                    total += angle;
+                    count++;
+                }
+            }
+            float average = total / count;
+            Quaternion rot2 = this.transform.rotation;
+            rot2 = rot2 * Quaternion.Euler(0, average+180, 0);
+            Vector3 space = new Vector3(0, 0, speed * Time.deltaTime);
+            space = rot2*space;
+            Rigidbody body = this.GetComponent<Rigidbody>();
+            body.AddForce(space, ForceMode.Impulse);
+            
+
+            
+            
+            total = 0;
+            count = 0;
+            foreach (GameObject raptor in GetComponentInParent<Flock>().units)
+            {
+
+                if (raptor != null && (this.transform.position - raptor.transform.position).magnitude > 10)
+                {
+                    Vector3 targetVector = raptor.transform.position - this.transform.position;
+                    Vector3 temp = new Vector3(0, 0, 1);
+                    temp = this.transform.rotation * temp;
+                    float angle = Vector3.SignedAngle(temp, targetVector, Vector3.up);
+                    total += angle;
+                    count++;
+                }
+            }
+
+            average = total / count;
+            rot2 = this.transform.rotation;
+            rot2 = rot2 * Quaternion.Euler(0, average, 0);
+            Vector3 space2 = new Vector3(0, 0, speed * Time.deltaTime);
+            space2 = rot2 * space2;
+            body = this.GetComponent<Rigidbody>();
+            if (average == double.NaN)
+            {
+                Debug.Log("I'm here");
+            }
+            body.AddForce(space2, ForceMode.Impulse);
+
+            
+            total = 0;
+            count = 0;
+            foreach (GameObject raptor in GetComponentInParent<Flock>().units)
+            {
+                //Vector3 targetVector = raptor.transform.rotation;// - this.transform.position;
+                if (raptor != null)
+                {
+                    Vector3 temp = new Vector3(0, 0, 1);
+                    Vector3 temp2 = new Vector3(0, 0, 1);
+                    temp = this.transform.rotation * temp;
+                    temp2 = raptor.transform.rotation * temp2;
+                    float angle = Vector3.SignedAngle(temp, temp2, Vector3.up);
+                    total += angle;
+                    count++;
+                }
+            }
+
+            average = total / count;
+
+            if (average > 0.25)
+            {
+                average = 0.25f;
+            }
+            else if (average < -0.25)
+            {
+                average = -0.25f;
+            }
+            Quaternion rot = this.transform.rotation;
+            rot = rot * Quaternion.Euler(0, average, 0);
+            this.transform.rotation = rot;
+
+            space = new Vector3(0, 0, speed * Time.deltaTime);
+            space = rot2 * space;
+            body = this.GetComponent<Rigidbody>();
+            body.AddForce(space, ForceMode.Impulse);
+
+        }
+
+
+    }
+
+}
+            /*
+            if ((this.transform.position - target.transform.position).magnitude > 10)
+            {
+                Vector3 targetVector = target.transform.position-this.transform.position;
+                Vector3 temp = new Vector3(0, 0, 1);
+                temp = this.transform.rotation*temp;
+                float angle = Vector3.SignedAngle(temp, targetVector,Vector3.up);
+                if (angle > 2)
+                {
+                    angle = 2;
+                }
+                else if (angle < -2)
+                {
+                    angle = -2;
+                }
+                Quaternion rot = this.transform.rotation;
+                rot = rot * Quaternion.Euler(0,angle,0);
+                this.transform.rotation = rot;
+
+                Vector3 myvec = new Vector3(0, 0, speed * Time.deltaTime);
+                myvec = this.transform.rotation * myvec;
+
+                Vector3 vec = this.transform.position;
+                
+                body.AddForce(myvec, ForceMode.Impulse);
+            }*/
---
+++ Assets/Resources/Scripts/Unit.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 pos, oldPos;
+    public Vector3 tarPos;
+
+    public int dmg;
+    public float t, attT;
+    public float range, attSpeed;
+    public float speed = 1.0F;
+    public GameObject target;
+    GameObject selector;
+    public bool deathTimer = false;
+    public bool isMoving = false;
+    public Animation anim;
+
+    void Start()
+    {
+        pos = tarPos = oldPos = this.transform.position;
+        t = Time.time;
+        selector=GameObject.FindWithTag("mainselector");
+        this.GetComponent<Stats>().health = this.GetComponent<Stats>().maxHealth;
+        this.anim = GetComponent<Animation>();
+        foreach (AnimationState state in anim)
+        {
+            state.speed = 0.5F;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.GetComponent<Stats>().health <= 0)
+        {
+            if (!deathTimer)
+            {
+                if (isMoving)
+                    anim.CrossFade("Move Die");
+                else
+                    anim.CrossFade("Stand Die");
+
+                deathTimer = true;
+            }
+            else
+            {
+                if (!anim.IsPlaying("Move Die") && !anim.IsPlaying("Stand Die"))
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+        else
+        {
+
+            if (target != null)
+            {
+                isMoving = true;
+                anim.CrossFade("Walk");
+                if (tarPos != target.transform.position)
+                {
+                    tarPos = target.transform.position;
+
+                    t = Time.time;
+                    pos = this.transform.position;
+                }
+
+                if (Vector3.Distance(transform.position, tarPos) <= range)
+                {
+                    tarPos = transform.position;
+                    isMoving = false;
+                    anim.CrossFade("Attack");
+
+                    if (this.GetComponent<HSplineMove>() != null)
+                    {
+                        Destroy(this.GetComponent<HSplineMove>().path[0]);
+                        Destroy(this.GetComponent<HSplineMove>().path[1]);
+                        Destroy(this.GetComponent<HSplineMove>());
+                    }
+
+                    if (Time.time - attT >= attSpeed)
+                    {
+                        attT = Time.time;
+                        target.GetComponent<Stats>().health -= dmg;
+
+                        if (gameObject.GetComponent<Stats>().type == 1 && target.GetComponent<Stats>().faction == 2)
+                        {
+                            target.GetComponent<Stats>().health -= (dmg * 4);
+                            Camera.main.GetComponent<PlayerScript>().eggs += (dmg * 5);
+                        }
+                    }
+                }
+            } else if (this.GetComponent<HSplineMove>()) {
+                isMoving = true;
+                anim.CrossFade("Walk");
+            }
+            else {
+                isMoving = false;
+                anim.CrossFade("Idle");
+            }
+        }
+    }
+
+    void OnMouseDown() {
+        if (this.GetComponent<Stats>().faction == 0)
+        {
+            GameObject oUsed = GameObject.FindWithTag("Selected");
+
+            if (oUsed != null)
+            {
+                oUsed.tag = "None";
+            }
+
+            this.gameObject.tag = "Selected";
+
+            Debug.Log(tarPos);
+        }
+        else if (this.GetComponent<Stats>().faction == 1)
+        {
+            GameObject oUsed = GameObject.FindWithTag("Selected");
+
+            if (oUsed != null)
+            {
+                StartCoroutine("moveSet");
+
+
+            }
+        }
+
+        else if (this.GetComponent<Stats>().faction == 2)
+        {
+            GameObject oUsed = GameObject.FindWithTag("Selected");
+
+            if (oUsed.GetComponent<Stats>().type == 1)
+            {
+                if (oUsed != null)
+                {
+                    StartCoroutine("moveSet");
+                }
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        this.transform.position = oldPos;
+
+        tarPos = transform.position;
+
+        if (this.GetComponent<HSplineMove>() != null)
+        {
+            Destroy(this.GetComponent<HSplineMove>().path[0]);
+            Destroy(this.GetComponent<HSplineMove>().path[1]);
+            Destroy(this.GetComponent<HSplineMove>());
+        }
+    }
+
+    IEnumerator moveSet()
+    {
+
+        Debug.Log("I'M HERE");
+        GameObject objUsed = GameObject.FindWithTag("Selected");
+        GameObject tempStart = Instantiate((GameObject)Resources.Load("Prefabs/Target", typeof(GameObject)), objUsed.transform.position, objUsed.transform.rotation);
+        GameObject tempEnd = Instantiate((GameObject)Resources.Load("Prefabs/Target", typeof(GameObject)), this.gameObject.transform.position, Quaternion.identity);
+
+        objUsed.GetComponent<Unit>().tarPos = this.gameObject.transform.position;
+        objUsed.GetComponent<Unit>().t = Time.time;
+        objUsed.GetComponent<Unit>().pos = GameObject.FindWithTag("Selected").GetComponent<Unit>().transform.position;
+        objUsed.GetComponent<Unit>().target = this.gameObject;
+
+        if (objUsed.GetComponent<HSplineMove>() != null)
+        {
+            Destroy(objUsed.GetComponent<HSplineMove>().path[0]);
+            Destroy(objUsed.GetComponent<HSplineMove>().path[1]);
+            Destroy(objUsed.GetComponent<HSplineMove>());
+            yield return new WaitForSeconds(0.05f);
+        }
+        objUsed.AddComponent(typeof(HSplineMove));
+        objUsed.GetComponent<HSplineMove>().speed = objUsed.GetComponent<Unit>().speed;
+        objUsed.GetComponent<HSplineMove>().path = new GameObject[2];
+        objUsed.GetComponent<HSplineMove>().path[0] = tempStart;
+        objUsed.GetComponent<HSplineMove>().path[1] = tempEnd;
+
+        yield return null;
+    }
+}
